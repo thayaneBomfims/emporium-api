d1daed69c592202ec9518387ddd4649c8f726eec
Thayane Bomfim <thayanesilbomfim@outlook.com>
2023-07-10 23:17:49 -0300

    prepare to deploy

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
   app.use(cors());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
